3050ef9e844bd2c06534c416c379f43d0fb8812d
Brandon R. Stoner <monokrome@limpidtech.com>
2014-07-09 22:27:14 -0700

    Add simple playlist support.

--- aural.go
+++ aural.go
@@ -2,10 +2,13 @@ package aural
 
 import (
 	"log"
+	"math/rand"
 
 	"code.google.com/p/portaudio-go/portaudio"
 	"github.com/mkb218/gosndfile/sndfile"
 )
+
+var FRAMES_PER_BUFFER = 8916
 
 func init() {
 	err := portaudio.Initialize()
@@ -15,21 +18,61 @@ func init() {
 	}
 }
 
-var FRAMES_PER_BUFFER = 8916
+type Track struct {
+	Location string
 
-func Play(fileName string) (err error) {
-	var info sndfile.Info
+	info *sndfile.Info
+	file *sndfile.File
+}
 
-	out := make([]int32, FRAMES_PER_BUFFER)
-	file, err := sndfile.Open(fileName, sndfile.Read, &info)
+func (track *Track) Load() error {
+	track.info = new(sndfile.Info)
+	file, err := sndfile.Open(track.Location, sndfile.Read, track.info)
 
 	if err != nil {
 		return err
 	}
 
+	track.file = file
+
+	return nil
+}
+
+type Playlist struct {
+	Tracks []Track
+}
+
+func (playlist *Playlist) Play() error {
+	for _, track := range playlist.Tracks {
+		if err := track.Load(); err != nil {
+			return err
+		}
+
+		if err := track.Play(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (playlist *Playlist) Shuffle() {
+	if len(playlist.Tracks) == 0 {
+		return
+	}
+
+	for i, j := len(playlist.Tracks)-1, 0; i > 0; i-- {
+		j = rand.Intn(i)
+		playlist.Tracks[i], playlist.Tracks[j] = playlist.Tracks[j], playlist.Tracks[i]
+	}
+}
+
+func (track *Track) Play() error {
+	out := make([]int32, FRAMES_PER_BUFFER)
+
 	stream, err := portaudio.OpenDefaultStream(
-		0, int(info.Channels),
-		float64(info.Samplerate),
+		0, int(track.info.Channels),
+		float64(track.info.Samplerate),
 		FRAMES_PER_BUFFER, &out)
 
 	if err != nil {
@@ -41,15 +84,15 @@ func Play(fileName string) (err error) {
 	stream.Start()
 	defer stream.Stop()
 
-	log.Printf("Playing %vKhz stream with %v channels.\n", info.Samplerate, info.Channels)
-
-	remaining := int64(1)
-
-	for remaining > 0 {
-		remaining, err = file.ReadFrames(out)
+	for {
+		remaining, err := track.file.ReadFrames(out)
 
 		if err != nil {
 			return err
+		}
+
+		if remaining == 0 {
+			break
 		}
 
 		stream.Write()
